b7ac76f50126df11fd0eb2674126bc2f1f178290
twisted <youdangls@gmail.com>
2020-09-04 22:04:55 +0800

    添加晚安曲机器人

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bot.utopia.cool
 the bot of website: utopia.cool
+
+现有以下几个小机器人：
+* 一言机器人：定时发送从一言官方接口获取的一句话文摘。
+* 晚安曲机器人：定时发送晚安曲推送。音乐文件及相关介绍需要管理员自行上传。
+
+这些机器人可以用于任何 Mastodon 网站。只需要修改代码中的 mstd_website 变量即可。
---
+++ night/bot_good_night.py
@@ -0,0 +1,143 @@
+import os
+import logging
+import schedule
+import time
+import linecache
+from datetime import datetime, timezone
+from mastodon import Mastodon, MastodonIllegalArgumentError
+
+# V0.0.9
+
+# init logger
+logger = logging.getLogger("good_night_bot")
+logger.setLevel(level=logging.DEBUG)
+handler = logging.FileHandler('good_night_bot.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# def website
+mstd_website = 'https://utopia.cool'
+
+# default music path
+FILE_PATH = os.getenv("NIGHT_BOT_FILE", "/home/bot/music")
+
+# account
+BOT_ACCOUNT = os.getenv("NIGHT_EMAIL", "")
+BOT_PASSWD = os.getenv("NIGHT_PASSWD", "")
+
+def make_cred_secret():
+    Mastodon.create_app(
+        'good night',
+        api_base_url = mstd_website,
+        to_file = 'pytooter_clientcred.secret'
+    )
+    mastodon = Mastodon(
+        client_id = 'pytooter_clientcred.secret',
+        api_base_url = mstd_website
+    )
+    mastodon.log_in(
+        BOT_ACCOUNT,
+        BOT_PASSWD,
+        to_file = 'pytooter_usercred.secret'
+    )
+
+def post_msg(text, media, schedule_time):
+    # To post, create an actual API instance.
+    mastodon = Mastodon(
+        access_token = 'pytooter_usercred.secret',
+        api_base_url = mstd_website
+    )
+    media_dict = mastodon.media_post(media)
+    response = mastodon.status_post("@twisted " + text, media_ids=media_dict, visibility="direct", scheduled_at=schedule_time)
+    return response
+
+def periodic_task():
+    # 这里似乎是UTC时间，不能直接用东8区时间。所以22:22的推送要减去8小时
+    schedule_time = datetime.today().replace(hour=14, minute=22, second=0, microsecond=0)
+    schedule_time = schedule_time.replace(tzinfo=timezone.utc)
+    try:
+        name, writer, music = get_music_info()
+    except:
+        send_error_msg("找不到介绍文字")
+        return
+
+    msg_text = get_msg_text(name, writer)
+
+    try:
+        music_file = get_music_path(music)
+    except:
+        send_error_msg("找不到音乐文件")
+        return
+    
+    for i in range(3):
+        try:
+            response = post_msg(msg_text, music_file, schedule_time)
+            print(response)
+        except:
+            print(i, "try fail")
+            logger.exception("periodic_task fail " + str(i))
+        else:
+            logger.info(get_date_str() + ' post done!')
+            break
+        if i == 2:
+            send_error_msg("文件及介绍无误，但发送失败")
+    
+def get_date():
+    d = datetime.today()
+    return d.year, d.month, d.day
+
+def get_date_str(separator='-'):
+    assert(isinstance(separator, str))
+    num = get_date()
+    day_str = separator.join(map(str, num))
+    return day_str
+
+def get_music_path(file_name):
+    music_file_path = os.path.join(FILE_PATH, file_name)
+    if not os.path.isfile(music_file_path):
+        raise IOError("没有找到今日音乐文件")
+    return music_file_path
+
+def get_music_info():
+    file_name = get_date_str() + '.txt'
+    info_file_path = os.path.join(FILE_PATH, file_name)
+    if not os.path.isfile(info_file_path):
+        raise IOError("没有找到今日info文件")
+    with open(info_file_path,encoding="utf-8") as f:
+        info = f.readlines()
+        name = info[0]
+        writer = info[1]
+        music = info[2]
+    return name, writer, music
+
+def get_msg_text(name, writer):
+    text = get_date_str('/')
+    text += "\n\n歌曲：" + name + "\n艺术家：" + writer
+    print(text)
+    return text
+
+def send_error_msg(text):
+    print(text)
+    # To post, create an actual API instance.
+    mastodon = Mastodon(
+        access_token = 'pytooter_usercred.secret',
+        api_base_url = mstd_website
+    )
+    msg = "@twisted @5432688 " + get_date_str() + '\n' + text
+    response = mastodon.status_post(msg, visibility="direct")
+    return response
+
+
+schedule.every().day.at("21:00").do(periodic_task)
+
+
+if __name__ == "__main__":
+    make_cred_secret()
+    print("periodic_task start!")
+    periodic_task()
+    
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(60)
+    print("finish")
