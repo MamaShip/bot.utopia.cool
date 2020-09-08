7c361eb7980bb11508feb7ea1ed5ff7c1a2c8e84
twisted <youdangls@gmail.com>
2020-09-08 18:53:15 +0800

    减少hitokoto发言频率

--- hitokoto/bot_hitokoto.py
+++ hitokoto/bot_hitokoto.py
@@ -96,12 +96,11 @@ def periodic_task():
 
 
 schedule.every().day.at("09:00").do(periodic_task)
-schedule.every().day.at("12:00").do(periodic_task)
-schedule.every().day.at("14:00").do(periodic_task)
-schedule.every().day.at("16:00").do(periodic_task)
+schedule.every().day.at("13:00").do(periodic_task)
+schedule.every().day.at("15:00").do(periodic_task)
+schedule.every().day.at("17:00").do(periodic_task)
 schedule.every().day.at("19:00").do(periodic_task)
-schedule.every().day.at("22:00").do(periodic_task)
-schedule.every().day.at("00:00").do(periodic_task)
+schedule.every().day.at("23:00").do(periodic_task)
 
 if __name__ == "__main__":
     make_cred_secret()
